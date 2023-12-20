4cfbb35d5dcb8ffac0638ee6a2d74068a91cb93d
foresec <109335369+foresec@users.noreply.github.com>
2023-12-20 22:01:46 +0900

    updated at 2023. 12. 20. 오후 10:01:46

--- baekjoon/2941/solution.py
+++ baekjoon/2941/solution.py
@@ -5,30 +5,10 @@ input = sys.stdin.readline
 ​
 cro = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
 ​
-# 공백 제거 필요
 word = input().rstrip()
+ans = 0
 ​
-ans = len(word) 
-idx = 0
+for c in cro:
+    ans += word.count(c)
 ​
-​
-while idx < len(word):   
-    check = False
-​
-    for j in range(idx, idx + 4):
-        
-        w = word[idx:j]        
-        if w in cro:
-            ans -= (len(w) -1)
-            idx += len(w)
-            check = True
-            break
-        else:
-            check = False
-​
-    if not check:
-        idx += 1
-​
-​
-print(ans)
-​
+print(len(word) - ans)
