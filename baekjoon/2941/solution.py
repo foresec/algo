053e139f1b33d93d611e77790b6712157aaefb2f
foresec <109335369+foresec@users.noreply.github.com>
2023-12-20 21:58:50 +0900

    updated at 2023. 12. 20. 오후 9:58:50

--- baekjoon/2941/solution.py
+++ baekjoon/2941/solution.py
@@ -1,23 +1,24 @@
 // [문제 링크]: https://www.acmicpc.net/problem/2941
 
 import sys
-​
 input = sys.stdin.readline
 ​
 cro = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
 ​
-word = input()
+# 공백 제거 필요
+word = input().rstrip()
 ​
-ans = len(word)
+ans = len(word) 
 idx = 0
 ​
-while idx < len(word):
+​
+while idx < len(word):   
     check = False
 ​
     for j in range(idx, idx + 4):
-        w = word[idx:j]
+        
+        w = word[idx:j]        
         if w in cro:
-​
             ans -= (len(w) -1)
             idx += len(w)
             check = True
@@ -29,5 +30,5 @@ while idx < len(word):
         idx += 1
 ​
 ​
-print(ans-1)
+print(ans)
 ​
