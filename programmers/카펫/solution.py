3ea18fa91e5c21a42375e15e14f4ab6a5758580e
foresec <109335369+foresec@users.noreply.github.com>
2023-12-22 16:34:32 +0900

    uploaded at 2023. 12. 22. 오후 4:34:32

---
+++ programmers/카펫/solution.py
@@ -0,0 +1,14 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3
+
+def solution(brown, yellow):
+    answer = []
+    
+    for i in range(yellow, 0, -1):
+        if yellow % i == 0:
+            a = i
+            b = yellow // i
+            if a + 1 + b + 1 == brown // 2:
+                answer = [a + 2, b + 2]
+                break
+                
+    return answer
