ab122ffaf71b834b4d6577755edaff9d696dc210
foresec <109335369+foresec@users.noreply.github.com>
2023-12-22 16:37:22 +0900

    updated at 2023. 12. 22. 오후 4:37:22

--- programmers/카펫/solution.py
+++ programmers/카펫/solution.py
@@ -3,13 +3,13 @@
 def solution(brown, yellow):
     answer = []
     
-    for i in range(yellow, 0, -1):
+    for i in range(1, yellow +1):
         if yellow % i == 0:
             a = i
             b = yellow // i
             
             if a + 1 + b + 1 == brown // 2:
-                answer = [a + 2, b + 2]
+                answer = [b + 2, a + 2]
                 break
                 
     return answer
