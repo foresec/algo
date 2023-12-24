a098381e7cb66a299bf5e79122b234648828e70c
foresec <109335369+foresec@users.noreply.github.com>
2023-12-25 04:21:19 +0900

    updated at 2023. 12. 25. 오전 4:21:19

--- baekjoon/7576/solution.py
+++ baekjoon/7576/solution.py
@@ -23,14 +23,13 @@ def bfs(tom):
                 continue
 ​
             # 익지 않은 토마토이고, 방문한적 없다면
-            if arr[nx][ny] == 0 and not visited[nx][ny]:
+            if arr[nx][ny] == 0:
                 q.append((nx, ny))
-                visited[nx][ny] = visited[x][y] + 1
+                arr[nx][ny] = arr[x][y] + 1
 ​
 ​
 M, N = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
 ans = 0
 ​
 tomato = []
@@ -38,19 +37,15 @@ for i in range(N):
     for j in range(M):
         if arr[i][j] == 1:
             tomato.append((i, j))
-            visited[i][j] = 1
 ​
 bfs(tomato)
-ans = max(max(row) for row in visited)
+ans = max(max(row) for row in arr)
 ​
-​
-# 안익은 사과가 남아있는지 탐색
-for i in range(N):
-    for j in range(M):
-        # 안익은 사과이면서 방문한적도 없다면(방문한적 없다 = 벽이거나, 벽에 가로막힌 사과 포함)
-        if arr[i][j] == 0 and visited[i][j] == 0:
-            print(-1)
-            sys.exit(0)
+# 안익은 토마토가 남아있는지 확인
+for row in arr:
+    if 0 in row:
+        print(-1)
+        sys.exit(0)
 ​
 print(ans - 1)
 ​
