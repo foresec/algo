d61fb71aae65a452d935540a944fd1733471cbaa
foresec <109335369+foresec@users.noreply.github.com>
2023-12-25 04:08:03 +0900

    updated at 2023. 12. 25. 오전 4:08:03

--- baekjoon/7576/solution.py
+++ baekjoon/7576/solution.py
@@ -1,57 +1,51 @@
 // [문제 링크]: https://www.acmicpc.net/problem/7576
 
-​
 import sys
-input = sys.stdin.readline
-​
 from collections import deque
+​
+input = sys.stdin.readline
 ​
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 ​
-​
 def bfs(tom):
     q = deque(tom)
-    day = 0
 ​
     while q:
-        # 포인트는 포문으로 첫 시작점을 모두 돌려야 함
-        for _ in range(len(q)):
-            x, y, day = q.popleft()
+        x, y = q.popleft()
                    
-            for d in range(4):
-                nx = x + dx[d]
-                ny = y + dy[d]
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
 ​
-                if not (0 <= nx < N and 0 <= ny < M):
-                    continue
+            if not (0 <= nx < N and 0 <= ny < M):
+                continue
 ​
-                if arr[nx][ny] == 0:
-                    q.append((nx, ny, day + 1))
-                    arr[nx][ny] = 1
-​
-    return day
+            if arr[nx][ny] == 0 and not visited[nx][ny]:
+                q.append((nx, ny))
+                visited[nx][ny] = visited[x][y] + 1
 ​
 ​
 M, N = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
 ans = 0
 ​
 tomato = []
 for i in range(N):
     for j in range(M):
-        # 토마토를 발견하면 익히기
         if arr[i][j] == 1:
-            # 시작 시점은 day = 0
-            tomato.append((i, j, 0))
+            tomato.append((i, j))
+            visited[i][j] = 1
 ​
-ans = bfs(tomato)
+bfs(tomato)
+ans = max(max(row) for row in visited)
 ​
 for i in range(N):
     for j in range(M):
-        if arr[i][j] == 0:
-            ans = -1
-            break
-        
+        if arr[i][j] == 0 and visited[i][j] == 0:
+            print(-1)
+            sys.exit(0)
 ​
-print(ans)
+print(ans - 1)  
+​
