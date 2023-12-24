2c707bc4edbefa8ccb5f68596d02ac801226e5ac
foresec <109335369+foresec@users.noreply.github.com>
2023-12-25 04:19:59 +0900

    updated at 2023. 12. 25. 오전 4:19:59

--- baekjoon/7576/solution.py
+++ baekjoon/7576/solution.py
@@ -8,12 +8,13 @@ input = sys.stdin.readline
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 ​
+​
 def bfs(tom):
     q = deque(tom)
 ​
     while q:
         x, y = q.popleft()
-                   
+​
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
@@ -21,6 +22,7 @@ def bfs(tom):
             if not (0 <= nx < N and 0 <= ny < M):
                 continue
 ​
+            # 익지 않은 토마토이고, 방문한적 없다면
             if arr[nx][ny] == 0 and not visited[nx][ny]:
                 q.append((nx, ny))
                 visited[nx][ny] = visited[x][y] + 1
@@ -41,11 +43,14 @@ for i in range(N):
 bfs(tomato)
 ans = max(max(row) for row in visited)
 ​
+​
+# 안익은 사과가 남아있는지 탐색
 for i in range(N):
     for j in range(M):
+        # 안익은 사과이면서 방문한적도 없다면(방문한적 없다 = 벽이거나, 벽에 가로막힌 사과 포함)
         if arr[i][j] == 0 and visited[i][j] == 0:
             print(-1)
             sys.exit(0)
 ​
-print(ans - 1)  
+print(ans - 1)
 ​
