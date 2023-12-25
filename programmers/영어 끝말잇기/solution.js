977171f87ace174a8a2f65f4061218c4f5bfe441
foresec <109335369+foresec@users.noreply.github.com>
2023-12-25 16:40:43 +0900

    updated at 2023. 12. 25. 오후 4:40:42

--- programmers/영어 끝말잇기/solution.js
+++ programmers/영어 끝말잇기/solution.js
@@ -6,7 +6,7 @@ function solution(n, words) {
     for (let i = 1; i < words.length ; i += 1) {
 
         // 끝말잇기가 성립이 안될때
-        if (words[i - 1][words[i - 1].length - 1] !== words[i][0] || words.slice(0, i).includes(words[i])) {
+        if (words[i-1][words[i-1].length-1] != words[i][0]) {
             let people = i % n
             let turn = Math.floor(i / n)
             answer = [people+1, turn+1]
@@ -15,13 +15,13 @@ function solution(n, words) {
         }
         
         // 중복된 단어가 있을 때
-//         if (words.slice(0, i).includes(words[i])) {
-//             let people = i % n
-//             let turn = Math.floor(i / n)
-//             answer = [people + 1, turn + 1]
-//             break
+        if (words.slice(0, i).includes(words[i])) {
+            let people = i % n
+            let turn = Math.floor(i / n)
+            answer = [people + 1, turn + 1]
+            break
             
-//         }
+        }
     }
 
     return answer;
