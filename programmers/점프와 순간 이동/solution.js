7e5b2f3dc25418f478e27ded98902f33101ba1b1
foresec <109335369+foresec@users.noreply.github.com>
2023-12-25 16:01:07 +0900

    uploaded at 2023. 12. 25. 오후 4:01:07

---
+++ programmers/점프와 순간 이동/solution.js
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12980?language=javascript
+
+function solution(n)
+{
+    // 맨처음 1이 필요
+    let ans = 1;
+
+    while (n > 1) {
+        // 나머지가 있으면 
+        if (n % 2 === 1) {
+            n -= 1
+            ans += 1
+        } else {
+            n = n / 2
+        }
+    }
+
+    return ans;
+}
