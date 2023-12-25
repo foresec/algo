837fb77ebe768e1d24bde4d6f03fe686646976fa
foresec <109335369+foresec@users.noreply.github.com>
2023-12-25 15:57:29 +0900

    updated at 2023. 12. 25. 오후 3:57:28

--- programmers/점프와 순간 이동/solution.py
+++ programmers/점프와 순간 이동/solution.py
@@ -4,17 +4,25 @@
 # 현재까지 온거리 * 2는 비용X
 # 이동거리 n이 주어졌을 때, 사용해야하는 건전지 사용량의 최솟값 구하기
 
-def solution(n):
-    ans = 1
+# def solution(n):
+#     ans = 1
     
-    while n > 1:
+#     while n > 1:
         
-        if n % 2 == 0:
-            n //= 2
+#         if n % 2 == 0:
+#             n //= 2
         
-        else:
-            n -= 1
-            ans += 1
+#         else:
+#             n -= 1
+#             ans += 1
     
 
+#     return ans
+
+
+# 위 답변도 맞지만
+# python에서 이진수로 바꿔주는 메서드가 있다. 이 메서드는 string형태로 반환함
+def solution(n):
+    ans = bin(n).count("1")    
     return ans
+
