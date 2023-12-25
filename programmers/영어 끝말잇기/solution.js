9100d8d33c3c35c346b97fdc17c7bf5bf566eb8f
foresec <109335369+foresec@users.noreply.github.com>
2023-12-25 16:26:33 +0900

    updated at 2023. 12. 25. 오후 4:26:32

--- programmers/영어 끝말잇기/solution.js
+++ programmers/영어 끝말잇기/solution.js
@@ -1,28 +1,49 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=javascript
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12981
 
-function solution(n, words) {
-    let answer = [0,0];
+// function solution(n, words) {
+//     let answer = [0,0];
 
-    for (let i = 1; i < words.length ; i += 1) {
+//     for (let i = 1; i < words.length ; i += 1) {
 
-        // 끝말잇기가 성립이 안될때
-        if (words[i-1][words[i-1].length-1] != words[i][0]) {
-            let people = i % n
-            let turn = Math.floor(i / n)
-            answer = [people+1, turn+1]
-            break
+//         // 끝말잇기가 성립이 안될때
+//         if (words[i-1][words[i-1].length-1] != words[i][0]) {
+//             let people = i % n
+//             let turn = Math.floor(i / n)
+//             answer = [people+1, turn+1]
+//             break
 
-        }
+//         }
         
-        // 중복된 단어가 있을 때
-        if (words.slice(0, i).includes(words[i])) {
-            let people = i % n
-            let turn = Math.floor(i / n)
-            answer = [people + 1, turn + 1]
-            break
+//         // 중복된 단어가 있을 때
+//         if (words.slice(0, i).includes(words[i])) {
+//             let people = i % n
+//             let turn = Math.floor(i / n)
+//             answer = [people + 1, turn + 1]
+//             break
             
-        }
-    }
+//         }
+//     }
 
-    return answer;
+//     return answer;
+// }
+
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+function solution(n, words) {
+    let answer = 0;
+    words.reduce((prev, now, idx) => {
+        answer = answer || ((words.slice(0, idx).indexOf(now) !== -1 || prev !== now[0]) ? idx : answer);
+        return now[now.length-1];
+    }, "")
+
+    return answer ? [answer%n+1, Math.floor(answer/n)+1] : [0,0];
 }
