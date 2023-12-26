93d5ac4b75bbd6346f5d8ff7b1b75dab648975ba
foresec <109335369+foresec@users.noreply.github.com>
2023-12-26 23:15:46 +0900

    updated at 2023. 12. 26. 오후 11:15:45

--- programmers/예상 대진표/solution.py
+++ programmers/예상 대진표/solution.py
@@ -7,17 +7,11 @@ def solution(n,a,b):
 
     while a != b:
         
-        if a % 2 == 1:
-            a //= 2
-            a += 1
-        else:
-            a //= 2
-            
-        if b % 2 == 1:
-            b //= 2
-            b += 1
-        else:
-            b //= 2
+        a += 1
+        b += 1
+        
+        a //= 2
+        b //= 2
         
         answer += 1
         
