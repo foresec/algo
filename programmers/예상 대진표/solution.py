6a5a5693693e8ebbe7e01c7b25e92d60146e9ea9
foresec <109335369+foresec@users.noreply.github.com>
2023-12-26 23:17:13 +0900

    updated at 2023. 12. 26. 오후 11:17:13

--- programmers/예상 대진표/solution.py
+++ programmers/예상 대진표/solution.py
@@ -1,17 +1,12 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=python3
 
-
-
 def solution(n,a,b):
     answer = 0
 
     while a != b:
         
-        a += 1
-        b += 1
-        
-        a //= 2
-        b //= 2
+        a = (a + 1) // 2
+        b = (b + 1) // 2        
         
         answer += 1
         
