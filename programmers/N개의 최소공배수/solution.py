310131e67b07be55b24fe5e5f20957e0090a2b1e
foresec <109335369+foresec@users.noreply.github.com>
2023-12-27 23:41:45 +0900

    updated at 2023. 12. 27. 오후 11:41:44

--- programmers/N개의 최소공배수/solution.py
+++ programmers/N개의 최소공배수/solution.py
@@ -1,46 +1,107 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12953?language=python3#
 
-def get_prime(n):
-    temp = [False, False] + [True] * (n-1)
-    primes = []
+# def get_prime(n):
+#     temp = [False, False] + [True] * (n-1)
+#     primes = []
     
-    for i in range(2, n+1):
-        if temp[i]:
-            primes.append(i)
-            for j in range(2 * i, n+1, i):
-                temp[j] = False
-    return primes
+#     for i in range(2, int(n**0.5) + 1):
+#         if temp[i]:
+#             primes.append(i)
+#             for j in range(i * i, n+1, i):
+#                 temp[j] = False
+                
+#     for i in range(max(2, int(n**0.5) + 1), n + 1):
+#         if temp[i]:
+#             primes.append(i)
+#     return primes
     
 
+# def solution(arr):
+#     answer = 1
+    
+    
+#     prime = get_prime(max(arr))
+#     idx = 0
+    
+#     while True:
+        
+#         cnt = 0
+#         for num in arr:
+#             if num == 1:
+#                 cnt += 1
+        
+#         if cnt == len(arr):
+#             break
+
+            
+#         check = False
+#         for i in range(len(arr)):
+#             if arr[i] % prime[idx] == 0:
+#                 arr[i] = arr[i] // prime[idx]
+#                 check = True
+        
+#         if check:
+#             answer *= prime[idx]
+#         else:
+#             idx += 1
+            
+    
+#     return answer
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
 def solution(arr):
     answer = 1
-    
-    
-    prime = get_prime(max(arr))
-    print(prime)
-    idx = 0
-    
+
+    now = 2
+
+    idx = 5
     while True:
-        
-        cnt = 0
+
         for num in arr:
             if num == 1:
-                cnt += 1
-        
-        if cnt == len(arr):
+                arr.remove(num)
+
+        if len(arr) == 0:
             break
 
-            
         check = False
         for i in range(len(arr)):
-            if arr[i] % prime[idx] == 0:
-                arr[i] = arr[i] // prime[idx]
+            if arr[i] % now == 0:
+                arr[i] = arr[i] // now
                 check = True
-        
+
         if check:
-            answer *= prime[idx]
+            answer *= now
         else:
-            idx += 1
-            
-    
+            now += 1
+
+
     return answer
