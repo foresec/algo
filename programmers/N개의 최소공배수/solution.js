19b0133731cf99049820203e3cf5e37127d5faaa
foresec <109335369+foresec@users.noreply.github.com>
2023-12-28 15:27:01 +0900

    updated at 2023. 12. 28. 오후 3:27:01

--- programmers/N개의 최소공배수/solution.js
+++ programmers/N개의 최소공배수/solution.js
@@ -1,50 +1,50 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12953?language=javascript
 
-// function solution(arr) {
-//     let answer = 1;
-//     let now = 2
-//     while (true) {
-        
-//         // 나누기
-//         let check = false
-//         for (let i=0; i < arr.length; i++) {
-//             if(arr[i] % now === 0) {
-//                 arr[i] /= now             
-//                 check = true
-//             }
-//         }
-        
-//         if (!check) {
-//             now += 1
-//         } else {
-//             answer *= now
-//         }
-        
-//         // 모든 arr의 요소들이 1이면
-//         if (arr.every((num => num === 1))) {
-//             break;
-//         }
-        
-//     }
-    
-//     return answer;
-// }
-
-
-function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
-}
-
-function lcm(a, b) {
-    return (a * b) / gcd(a, b);
-}
-
 function solution(arr) {
     let answer = 1;
-
-    for (let num of arr) {
-        answer = lcm(answer, num);
+    let now = 2
+    while (true) {
+        
+        // 나누기
+        let check = false
+        for (let i=0; i < arr.length; i++) {
+            if(arr[i] % now === 0) {
+                arr[i] /= now             
+                check = true
+            }
+        }
+        
+        if (!check) {
+            now += 1
+        } else {
+            answer *= now
+        }
+        
+        // 모든 arr의 요소들이 1이면
+        if (arr.every((num => num === 1))) {
+            break;
+        }
+        
     }
-
+    
     return answer;
 }
+
+
+// function gcd(a, b) {
+//     return b === 0 ? a : gcd(b, a % b);
+// }
+
+// function lcm(a, b) {
+//     return (a * b) / gcd(a, b);
+// }
+
+// function solution(arr) {
+//     let answer = 1;
+
+//     for (let num of arr) {
+//         answer = lcm(answer, num);
+//     }
+
+//     return answer;
+// }
