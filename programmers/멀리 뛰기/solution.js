e0625869f3fcc2e17a2d9906b04f40552f820e48
foresec <109335369+foresec@users.noreply.github.com>
2023-12-28 16:33:30 +0900

    updated at 2023. 12. 28. 오후 4:33:29

--- programmers/멀리 뛰기/solution.js
+++ programmers/멀리 뛰기/solution.js
@@ -1,22 +1,41 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12914?language=javascript
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12914
 
-// 계산시 피보나치와 동일하게 증가
+// // 계산시 피보나치와 동일하게 증가
+// function solution(n) {
+
+//     // 2 이하면 n 반환
+//     if (n <= 2) {
+//         return n
+//     }
+    
+//     // n+1길이의 배열에 0을 채움
+//     const dp =  Array.from({length: n+1}, () => 0)
+    
+//     dp[1] = 1
+//     dp[2] = 2
+    
+//     for (let i = 3; i <= n; i++) {
+//         dp[i] = (dp[i-1] + dp[i-2])
+//     }
+
+//     return dp[n]  % 1234567;
+// }
+
+
 function solution(n) {
-
-    // 2 이하면 n 반환
     if (n <= 2) {
-        return n
-    }
-    
-    // n+1길이의 배열에 0을 채움
-    const dp =  Array.from({length: n+1}, () => 0)
-    
-    dp[1] = 1
-    dp[2] = 2
-    
-    for (let i = 3; i <= n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+        return n;
     }
 
-    return dp[n];
+    const dp = Array.from({ length: n + 1 }, () => 0n); // 0n으로 BigInt 배열 초기화
+    dp[1] = 1n;
+    dp[2] = 2n;
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) ; // 나머지 연산 중에도 BigInt 사용
+    }
+
+    return Number(dp[n] % 1234567n); // BigInt를 다시 일반적인 Number로 변환
 }
+
+console.log(solution(1000)); // 예상 결과: 662387
