787de6c2061f28f0d98318bfbe92b573184edeaa
foresec <109335369+foresec@users.noreply.github.com>
2023-12-28 18:57:49 +0900

    updated at 2023. 12. 28. 오후 6:57:49

--- baekjoon/1068/solution.js
+++ baekjoon/1068/solution.js
@@ -10,6 +10,8 @@ const removeNode = parseInt(input[2]);
 ​
 const removeEffect = (remove) => {
     parent[remove] = -2;
+​
+    // 삭제노드를 부모로 가진 모든 자식노드의 부모를 재귀를 통해 -2로 업데이트
     for (let i = 0; i < N; i++) {
             if (remove === parent[i]) {
                     removeEffect(i);
@@ -19,9 +21,13 @@ const removeEffect = (remove) => {
 ​
 removeEffect(removeNode);
 ​
+// 해당 노드의 부모가 삭제 되지 않았음 && 해당 노드의 자식노드가 존재하지 않을 때 
 let cnt = 0
+​
+const parentSet = new Set(parent);
+​
 for (let i = 0; i < N; i++) {
-    if (parent[i] !== -2 && !parent.includes(i)) {
+    if (parent[i] !== -2 && !parentSet.has(i)) {
         cnt += 1;
     }
 }
