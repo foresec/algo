1e8d59abbf60cd918c603b6ce3a42c96116eae04
foresec <109335369+foresec@users.noreply.github.com>
2023-12-29 18:47:06 +0900

    updated at 2023. 12. 29. 오후 6:47:06

--- programmers/n^2 배열 자르기/solution.py
+++ programmers/n^2 배열 자르기/solution.py
@@ -1,13 +1,24 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=python3
 
-def solution(n, left, right):
-    answer = []
+# def solution(n, left, right):
+#     answer = []
     
-    for i in range(left, right + 1):
-        row = i // n
-        col = i % n
-        temp = max(row, col) + 1
-        answer.append(temp)
+#     for i in range(left, right + 1):
+#         row = i // n
+#         col = i % n
+#         temp = max(row, col) + 1
+#         answer.append(temp)
     
-    return answer
+#     return answer
 
+def solution(n, left, right):
+    size = right - left + 1
+    answer = [0] * size
+
+    for i in range(size):
+        index = left + i
+        row = index // n
+        col = index % n
+        answer[i] = max(row, col) + 1
+
+    return answer
