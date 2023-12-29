d58ff118945c656e022f62ec124e75034ce302e6
foresec <109335369+foresec@users.noreply.github.com>
2023-12-29 18:05:15 +0900

    uploaded at 2023. 12. 29. 오후 6:05:15

---
+++ programmers/괄호 회전하기/solution.js
@@ -0,0 +1,45 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=javascript
+
+function solution(s) {
+    
+    let answer = 0;
+    const len_s = s.length
+    
+    let complete = new Map()
+    complete.set('(', ')')
+    complete.set('{', '}')
+    complete.set('[', ']')
+    
+    
+    
+    for (let i=0; i < len_s; i++) {
+        
+        let stack = []
+        for (let j = 0; j < len_s; j++) {
+            let temp = s[(i+j) % len_s]
+            
+
+            if (stack.length === 0) {
+                stack.push(temp)
+                continue
+            }
+        
+                     
+            if (complete.has(stack[stack.length - 1]) && complete.get(stack[stack.length-1]) === temp) {
+                stack.pop()
+            } else {
+                stack.push(temp)
+            }
+    
+            
+        }
+         
+        if (stack.length === 0) {
+            answer++
+        }
+        
+    }
+    
+    
+    return answer;
+}
