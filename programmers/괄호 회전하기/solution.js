e942d6ce881af7b87141bb41e186e363e693be2b
foresec <109335369+foresec@users.noreply.github.com>
2023-12-29 18:09:50 +0900

    updated at 2023. 12. 29. 오후 6:09:50

--- programmers/괄호 회전하기/solution.js
+++ programmers/괄호 회전하기/solution.js
@@ -14,23 +14,24 @@ function solution(s) {
     
     for (let i=0; i < len_s; i++) {
         
-        // if ( ["]", "}", ")"].includes(s[i]) ) {
-        //     continue
-        // }
+        // (추가적으로) 처음부터 닫혀있을 경우 성립할 수 없으니 continue 
+        if ( ["]", "}", ")"].includes(s[i]) ) {
+            continue
+        }
         
         
         let stack = []
         for (let j = 0; j < len_s; j++) {
             let temp = s[(i+j) % len_s]
             
-
+            // stack이 없을 경우 추가하고 push 후 continue
             if (!stack.length) {
                 stack.push(temp)
                 continue
             }
         
-                     
-            if (complete.has(stack[stack.length - 1]) && complete.get(stack[stack.length-1]) === temp) {
+            // 해당 괄호가 존재하고 짝이 맞추         
+            if (complete.get(stack[stack.length-1]) === temp) {
                 stack.pop()
             } else {
                 stack.push(temp)
