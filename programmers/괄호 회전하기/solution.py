82a5a4219c77664828139f24940418325a240ea0
foresec <109335369+foresec@users.noreply.github.com>
2023-12-29 17:08:53 +0900

    uploaded at 2023. 12. 29. 오후 5:08:53

---
+++ programmers/괄호 회전하기/solution.py
@@ -0,0 +1,18 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=python3
+
+def solution(s):
+    answer = 0
+    len_s = len(s)
+    
+    for i in range(len_s):
+        stack = []
+        for j in range(i,len_s + i):
+            temp = s[j % len_s]
+            if stack and ((stack[-1] == "(" and temp == ")" ) or (stack[-1] == "{" and temp == "}") or (stack[-1] == "[" and temp == "]")):
+                stack.pop()
+            else:
+                stack.append(temp)          
+        if not stack:
+            answer += 1
+                               
+    return answer
