b01cb086fe406a048fb40e0ab2b9ccbd09b73736
foresec <109335369+foresec@users.noreply.github.com>
2023-12-30 23:47:02 +0900

    updated at 2023. 12. 30. 오후 11:47:02

--- programmers/JadenCase 문자열 만들기/solution.py
+++ programmers/JadenCase 문자열 만들기/solution.py
@@ -1,4 +1,4 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12951#
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12951
 
 def solution(s):
     answer = ''
