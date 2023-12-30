ff693be388c1babf3bf85daefbc413c96efe302d
foresec <109335369+foresec@users.noreply.github.com>
2023-12-31 00:50:13 +0900

    updated at 2023. 12. 31. 오전 12:50:13

--- programmers/숫자의 표현/solution.py
+++ programmers/숫자의 표현/solution.py
@@ -1,17 +1,31 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12924
 
-def solution(n):
-    answer = 1
+# def solution(n):
+#     answer = 1
     
-    for i in range(n // 2 +1, 0, -1):
-        temp = i
-        for j in range(i-1, 0, -1):
-            temp += j
+#     for i in range(n // 2 +1, 0, -1):
+#         temp = i
+#         for j in range(i-1, 0, -1):
+#             temp += j
             
-            if temp > n:
+#             if temp > n:
+#                 break
+#             elif temp == n:
+#                 answer += 1
+#                 break
+                
+#     return answer
+
+
+def solution(num):
+    answer = 0
+    for i in range(1, num + 1):
+        sum = 0
+        for j in range(i, num + 1):
+            sum += j
+            if sum > num:
                 break
-            elif temp == n:
+            elif sum == num:
                 answer += 1
                 break
-                
     return answer
