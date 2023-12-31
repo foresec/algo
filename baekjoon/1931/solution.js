11cebb03eb2c6a2aaa2eb02dba941cc95817c6e6
foresec <109335369+foresec@users.noreply.github.com>
2023-12-31 21:38:27 +0900

    uploaded at 2023. 12. 31. 오후 9:38:27

---
+++ baekjoon/1931/solution.js
@@ -0,0 +1,33 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1931
+
+// 사용할 수 있는 회의의 최대 개수
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./1931.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+​
+const N = parseInt(input[0]);
+const arr = input.slice(1).map((line) => line.split(" ").map(Number));
+​
+// 끝나는 시간, 시작시간 순으로 오름차순으로 정렬
+arr.sort((a, b) => {
+    if (a[1] === b[1]) {
+        return a[0] - b[0];
+    } else {
+        return a[1] - b[1];
+    }
+});
+​
+// arr.sort((a, b) => a[1] - b[1] || a[0] - b[0]);
+​
+​
+let cnt = 1;
+let now = arr[0][1];
+​
+for (let i = 1; i < N; i++) {
+        if (now <= arr[i][0]) {
+                now = arr[i][1];
+                cnt += 1;
+        }
+}
+console.log(cnt);
+​
