170919225bd32019a2cf20186cffbff939e698ba
foresec <109335369+foresec@users.noreply.github.com>
2023-12-31 21:46:07 +0900

    updated at 2023. 12. 31. 오후 9:46:07

--- baekjoon/1931/solution.js
+++ baekjoon/1931/solution.js
@@ -10,24 +10,25 @@ const arr = input.slice(1).map((line) => line.split(" ").map(Number));
 ​
 // 끝나는 시간, 시작시간 순으로 오름차순으로 정렬
 arr.sort((a, b) => {
-    if (a[1] === b[1]) {
-        return a[0] - b[0];
-    } else {
-        return a[1] - b[1];
-    }
+  if (a[1] !== b[1]) {
+    return a[1] - b[1];
+  } else {
+    return a[0] - b[0];
+  }
 });
 ​
-// arr.sort((a, b) => a[1] - b[1] || a[0] - b[0]);
-​
+// arr.sort((a, b) => {
+//  return a[1] - b[1] || a[0] - b[0];
+// });
 ​
 let cnt = 1;
 let now = arr[0][1];
 ​
 for (let i = 1; i < N; i++) {
-        if (now <= arr[i][0]) {
-                now = arr[i][1];
-                cnt += 1;
-        }
+  if (now <= arr[i][0]) {
+    now = arr[i][1];
+    cnt += 1;
+  }
 }
 console.log(cnt);
 ​
