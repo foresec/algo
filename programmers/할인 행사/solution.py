caf6763329d508612f30261ee65fd06557819417
foresec <109335369+foresec@users.noreply.github.com>
2024-01-01 20:45:56 +0900

    uploaded at 2024. 1. 1. 오후 8:45:55

---
+++ programmers/할인 행사/solution.py
@@ -0,0 +1,26 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/131127
+
+# 10일 동안 회원자격 부여
+# 가능한 날의 갯수를 구해야함
+
+def solution(want, number, discount):
+    answer = 0
+    total = sum(number)
+    
+    food_dict = dict(zip(want, number))
+    
+    for i in range(len(discount)-9):
+        temp = discount[i:i+10]
+        cnt = 0
+        
+        for key, val in food_dict.items():
+            if temp.count(key) != val:
+                break
+            else:
+                cnt += 1
+                
+        if cnt == len(want): 
+            answer += 1
+
+
+    return answer
