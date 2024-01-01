220d6e9e7bfc8f8e2bfb75791f6f43921244cab7
foresec <109335369+foresec@users.noreply.github.com>
2024-01-01 21:47:15 +0900

    updated at 2024. 1. 1. 오후 9:47:14

--- programmers/H-Index/solution.js
+++ programmers/H-Index/solution.js
@@ -1,24 +1,24 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=javascript
 
-function solution(citations) {
-    let answer = 0;
-    // 최댓값을 찾아야 하므로 내림차순 정렬 후
-    citations.sort((a,b) => b - a)
-        
-    // 인용횟수가 h인덱스 이상일 때 update
-    for (let i=1; i < citations.length + 1;i++) {
-        if (citations[i-1] >= i){
-            answer ++
-        }
-    }
-    return answer;
-}
-
-
 // function solution(citations) {
-//     citations.sort((a, b) => b - a);
-
-//     const answer = citations.filter((citation, index) => citation >= index + 1).length;
-
+//     let answer = 0;
+//     // 최댓값을 찾아야 하므로 내림차순 정렬 후
+//     citations.sort((a,b) => b - a)
+        
+//     // 인용횟수가 h인덱스 이상일 때 update
+//     for (let i=1; i < citations.length + 1;i++) {
+//         if (citations[i-1] >= i){
+//             answer ++
+//         }
+//     }
 //     return answer;
 // }
+
+
+function solution(citations) {
+    citations.sort((a, b) => b - a);
+
+    const answer = citations.filter((citation, index) => citation >= index + 1).length;
+
+    return answer;
+}
