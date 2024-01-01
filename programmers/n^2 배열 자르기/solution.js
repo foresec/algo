f36cc5a23e3a3c2a770b824fc2bbdfca746f548b
foresec <109335369+foresec@users.noreply.github.com>
2024-01-01 22:02:07 +0900

    updated at 2024. 1. 1. 오후 10:02:07

--- programmers/n^2 배열 자르기/solution.js
+++ programmers/n^2 배열 자르기/solution.js
@@ -1,6 +1,6 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=javascript
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87390
 
-//틀림(실패 (signal: aborted (core dumped)) )
+// // 틀림(실패 (signal: aborted (core dumped)) )
 // function solution(n, left, right) {
     
 //     const temp = []
@@ -17,34 +17,35 @@
 // }
 
 
-function solution(n, left, right) {
-    const answer = []
-    // 한줄로 다 넣기
-    for (let i = left; i <= right; i++) {
-        const row = Math.floor(i / n)
-        const col = i % n;
-        const value = Math.max(row+1, col+1);
-        answer.push(value);
-    }
-    return answer;
-}
-
-
 // function solution(n, left, right) {
-//     const size = right - left + 1
-//     const answer = Array(size).fill(0)
-
+//     const answer = []
 //     // 한줄로 다 넣기
-//     for (i=0;i<size;i++) {
-//         let temp = left + i
-//         const row = Math.floor(temp / n)
-//         const col = temp % n
-//         const val = Math.max(row, col) + 1
-//         answer[i] = val        
+//     for (let i = left; i <= right; i++) {
+//         const row = Math.floor(i / n)
+//         const col = i % n;
+//         const value = Math.max(row+1, col+1);
+//         answer.push(value);
 //     }
-
 //     return answer;
 // }
+
+
+function solution(n, left, right) {
+    const size = right - left + 1
+    const answer = Array(size).fill(0)
+
+    // 한줄로 다 넣기
+    for (i=0;i<size;i++) {
+        let temp = left + i
+        const row = Math.floor(temp / n)
+        const col = temp % n
+        const val = Math.max(row, col) + 1
+        answer[i] = val        
+    }
+    console.log(answer.slice(2,2))
+
+    return answer;
+}
 
 
 
