de0368a01deb17a387ca0aad4836c825cf535f9b
foresec <109335369+foresec@users.noreply.github.com>
2024-01-01 21:48:08 +0900

    updated at 2024. 1. 1. 오후 9:48:07

--- programmers/H-Index/solution.py
+++ programmers/H-Index/solution.py
@@ -1,33 +1,5 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3
 
-# def solution(citations):
-#     answer = 0
-#     len_c = len(citations)
-#     citations.sort(reverse = True)
-    
-#     max_v = max(citations)
-#     check = 0
-#     idx = 0
-    
-#     while idx < len_c:
-    
-#         for i in range(len_c):
-#             if citations[i] >= max_v:
-#                 check += 1
-        
-#         if max_v == check:
-#             answer = max_v
-#             break
-            
-#         check = 0
-        
-#         idx += 1
-#         max_v = citations[idx] 
-
-            
-    
-#     return answer
-
 
 def solution(citations):
     answer = 0
