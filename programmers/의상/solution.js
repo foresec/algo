79ebfbf9f637d8b821f64d339e6ba1fc3616fe8e
foresec <109335369+foresec@users.noreply.github.com>
2024-01-02 15:01:28 +0900

    uploaded at 2024. 1. 2. 오후 3:01:27

---
+++ programmers/의상/solution.js
@@ -0,0 +1,37 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=javascript
+
+function solution(clothes) {
+    let answer = 0;
+    let cloMap = new Map()
+    for (let item of clothes) {
+        // 없으면 key, value
+        if (!cloMap.has(item[1])) {
+            cloMap.set(item[1], 1)
+        } else {
+            // 있으면 value +1
+            let tempVal = cloMap.get(item[1])
+            cloMap.set(item[1], tempVal + 1)
+        }    
+    }
+    
+    // 각 카테고리의 갯수 + 1을 해서 입지 않는 경우를 더해줌
+    let cnt = 1
+    for (val of cloMap.values()) {
+        cnt *= (val + 1)
+    }
+    
+    // 마지막으로 하나도 입지 않는 경우 1가지를 뺌
+    answer += (cnt - 1)
+    
+    
+
+    
+    return answer;
+}
+
+
+    
+    // 기본적으로 이렇게 바로 배열을 Map형태로 바꿀 수 있음
+    // let cloMap = new Map(clothes)
+    // map메서드를 활용하여 key, value값 변환
+    // let cloMap = new Map(clothes.map( item => [item[1], item[0]]));
