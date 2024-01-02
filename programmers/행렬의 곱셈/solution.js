443bc9e30c2b1fe7b69bf3a495c0a1993066eb4b
foresec <109335369+foresec@users.noreply.github.com>
2024-01-02 14:14:47 +0900

    updated at 2024. 1. 2. 오후 2:14:47

--- programmers/행렬의 곱셈/solution.js
+++ programmers/행렬의 곱셈/solution.js
@@ -3,29 +3,29 @@
 // 1
 // 1,2의 속도차이가 막 두드러지게 나진 않음
 
-// function solution(arr1, arr2) {
-        
-//     let row_len = arr1.length
-//     let col_len = arr2[0].length
-//     let k_len = arr2.length
-    
-//     let answer = Array(row_len).fill(0).map(() => Array(col_len).fill(0));
-    
-//     for (let i = 0; i < row_len; i++) {
-//         for (let j = 0 ; j < col_len; j++) {
-
-//             for (let k = 0; k < k_len; k++) {
-//                 answer[i][j] += arr1[i][k] * arr2[k][j]
-//             }
-//         }
-        
-//     }
-//     return answer;
-// }
-
-
-
-// 2
 function solution(arr1, arr2) {
-    return arr1.map((row) => arr2[0].map((x,y) => row.reduce((a,b,c) => a + b * arr2[c][y], 0)))
+        
+    let row_len = arr1.length
+    let col_len = arr2[0].length
+    let k_len = arr2.length
+    
+    let answer = Array(row_len).fill(0).map(() => Array(col_len).fill(0));
+    
+    for (let i = 0; i < row_len; i++) {
+        for (let j = 0 ; j < col_len; j++) {
+
+            for (let k = 0; k < k_len; k++) {
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+            }
+        }
+        
+    }
+    return answer;
 }
+
+
+
+// 2 참고풀이
+// function solution(arr1, arr2) {
+//     return arr1.map((row) => arr2[0].map((x,y) => row.reduce((a,b,c) => a + b * arr2[c][y], 0)))
+// }
