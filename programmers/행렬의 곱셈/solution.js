8c5d6b57ae50a1be74432f7c105f0d7cabd51061
foresec <109335369+foresec@users.noreply.github.com>
2024-01-02 14:08:39 +0900

    updated at 2024. 1. 2. 오후 2:08:39

--- programmers/행렬의 곱셈/solution.js
+++ programmers/행렬의 곱셈/solution.js
@@ -1,21 +1,26 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=javascript
 
-function solution(arr1, arr2) {
+// function solution(arr1, arr2) {
         
-    let row_len = arr1.length
-    let col_len = arr2[0].length
-    let k_len = arr2.length
+//     let row_len = arr1.length
+//     let col_len = arr2[0].length
+//     let k_len = arr2.length
     
-    let answer = Array(row_len).fill(0).map(() => Array(col_len).fill(0));
+//     let answer = Array(row_len).fill(0).map(() => Array(col_len).fill(0));
     
-    for (let i = 0; i < row_len; i++) {
-        for (let j = 0 ; j < col_len; j++) {
+//     for (let i = 0; i < row_len; i++) {
+//         for (let j = 0 ; j < col_len; j++) {
 
-            for (let k = 0; k < k_len; k++) {
-                answer[i][j] += arr1[i][k] * arr2[k][j]
-            }
-        }
+//             for (let k = 0; k < k_len; k++) {
+//                 answer[i][j] += arr1[i][k] * arr2[k][j]
+//             }
+//         }
         
-    }
-    return answer;
+//     }
+//     return answer;
+// }
+
+
+function solution(arr1, arr2) {
+    return arr1.map((row) => arr2[0].map((x,y) => row.reduce((a,b,c) => a + b * arr2[c][y], 0)))
 }
