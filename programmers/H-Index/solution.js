fa73bc7389161a4a66bb7e2958e66aab76c9ea9b
foresec <109335369+foresec@users.noreply.github.com>
2024-01-02 15:39:24 +0900

    updated at 2024. 1. 2. 오후 3:39:24

--- programmers/H-Index/solution.js
+++ programmers/H-Index/solution.js
@@ -1,18 +1,18 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=javascript
 
-// function solution(citations) {
-//     let answer = 0;
-//     // 최댓값을 찾아야 하므로 내림차순 정렬 후
-//     citations.sort((a,b) => b - a)
+function solution(citations) {
+    let answer = 0;
+    // 최댓값을 찾아야 하므로 내림차순 정렬 후
+    citations.sort((a,b) => b - a)
         
-//     // 인용횟수가 h인덱스 이상일 때 update
-//     for (let i=1; i < citations.length + 1;i++) {
-//         if (citations[i-1] >= i){
-//             answer ++
-//         }
-//     }
-//     return answer;
-// }
+    // 인용횟수가 h인덱스 이상일 때 update
+    for (let i=1; i < citations.length + 1;i++) {
+        if (citations[i-1] >= i){
+            answer ++
+        } 
+    }
+    return answer;
+}
 
 
 // function solution(citations) {
@@ -22,23 +22,3 @@
 
 //     return answer;
 // }
-
-function solution(citations) {
-    let start = 0;
-    let end = citations.length;
-    let answer = 0;
-    
-    while (start <= end) {
-        const mid = parseInt((start + end) / 2);
-        const count = citations.filter(v => v >= mid).length;
-
-        if (count >= mid) {
-            answer = mid;
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
-    }
-    
-    return answer;
-}
