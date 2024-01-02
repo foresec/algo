400695660119388078587c6efd3bea703ac5600a
foresec <109335369+foresec@users.noreply.github.com>
2024-01-02 14:12:42 +0900

    updated at 2024. 1. 2. 오후 2:12:41

--- programmers/행렬의 곱셈/solution.js
+++ programmers/행렬의 곱셈/solution.js
@@ -1,31 +1,31 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=javascript
 
-// 1
-// 2번이 더 빠른경우도 있긴한데 거의 1번풀이가 더 빠른듯
+// // 1
+// // 2번이 더 빠른경우도 있긴한데 거의 1번풀이가 더 빠른듯
 
-function solution(arr1, arr2) {
-        
-    let row_len = arr1.length
-    let col_len = arr2[0].length
-    let k_len = arr2.length
-    
-    let answer = Array(row_len).fill(0).map(() => Array(col_len).fill(0));
-    
-    for (let i = 0; i < row_len; i++) {
-        for (let j = 0 ; j < col_len; j++) {
-
-            for (let k = 0; k < k_len; k++) {
-                answer[i][j] += arr1[i][k] * arr2[k][j]
-            }
-        }
-        
-    }
-    return answer;
-}
-
-
-
-// 2
 // function solution(arr1, arr2) {
-//     return arr1.map((row) => arr2[0].map((x,y) => row.reduce((a,b,c) => a + b * arr2[c][y], 0)))
+        
+//     let row_len = arr1.length
+//     let col_len = arr2[0].length
+//     let k_len = arr2.length
+    
+//     let answer = Array(row_len).fill(0).map(() => Array(col_len).fill(0));
+    
+//     for (let i = 0; i < row_len; i++) {
+//         for (let j = 0 ; j < col_len; j++) {
+
+//             for (let k = 0; k < k_len; k++) {
+//                 answer[i][j] += arr1[i][k] * arr2[k][j]
+//             }
+//         }
+        
+//     }
+//     return answer;
 // }
+
+
+
+2
+function solution(arr1, arr2) {
+    return arr1.map((row) => arr2[0].map((x,y) => row.reduce((a,b,c) => a + b * arr2[c][y], 0)))
+}
