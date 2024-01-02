0cf462c2debf926db5725ed0768b8fe54255447a
foresec <109335369+foresec@users.noreply.github.com>
2024-01-02 22:40:15 +0900

    uploaded at 2024. 1. 2. 오후 10:40:14

---
+++ baekjoon/7576/solution.js
@@ -0,0 +1,71 @@
+// [문제 링크]: https://www.acmicpc.net/problem/7576
+
+const fs = require("fs");
+​
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./7576.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+​
+const dx = [1, 0, -1, 0];
+const dy = [0, -1, 0, 1];
+​
+function bfs(tom) {
+  const q = [...tom];
+    let head = 0
+  while (q.length > head) {
+    const [x, y] = q[head];
+​
+    for (let d = 0; d < 4; d++) {
+      const nx = x + dx[d];
+      const ny = y + dy[d];
+​
+      if (!(0 <= nx && nx < N && 0 <= ny && ny < M)) {
+        continue;
+      }
+​
+      // 안익은 토마토일 때 1씩 더해감
+      if (arr[nx][ny] === 0) {
+        q.push([nx, ny]);
+        arr[nx][ny] = arr[x][y] + 1;
+      }
+    }
+        head++
+  }
+}
+​
+const [M, N] = input[0].split(" ").map(Number);
+const arr = input.slice(1).map((line) => line.split(" ").map(Number));
+​
+let ans = 0;
+​
+const tomato = [];
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (arr[i][j] === 1) {
+      tomato.push([i, j]);
+    }
+  }
+}
+​
+bfs(tomato);
+ans = Math.max(...arr.map((row) => Math.max(...row)))-1;
+​
+// 안익은 토마토가 남아있는지 확인
+​
+check = true
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    // 익지 않은 토마토가 있음
+    if (arr[i][j] === 0) {
+​
+            check = false
+            break
+    }
+  }
+}
+if (check) {
+    console.log(ans)
+} else {
+    console.log(-1)
+}
+​
+​
