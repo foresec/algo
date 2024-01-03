4694754db3e924f310db6bc704445b5f0621bd66
foresec <109335369+foresec@users.noreply.github.com>
2024-01-03 22:43:00 +0900

    updated at 2024. 1. 3. 오후 10:43:00

--- programmers/튜플/solution.py
+++ programmers/튜플/solution.py
@@ -1,24 +1,24 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=python3
 
-# 붙여나감
-def solution(s):
-    answer = []
-    # split을 잘하고
-    first = s[2:-2].split("},{")
-    second = []
-    for f in first:
-        a = list(map(int, f.split(",")))
-        second.append(a)
+# # 붙여나감
+# def solution(s):
+#     answer = []
+#     # split을 잘하고
+#     first = s[2:-2].split("},{")
+#     second = []
+#     for f in first:
+#         a = list(map(int, f.split(",")))
+#         second.append(a)
     
-    # 가장 작은 길이부터 sort해서 not in 으로 채워나가는걸 찾는다??(append)
-    second.sort(key= lambda x: len(x))
+#     # 가장 작은 길이부터 sort해서 not in 으로 채워나가는걸 찾는다??(append)
+#     second.sort(key= lambda x: len(x))
     
-    answer.append(second[0][0])
-    for i in range(1, len(second)):
-        b = set(second[i]) - set(second[i-1])
-        answer.append(b.pop())
+#     answer.append(second[0][0])
+#     for i in range(1, len(second)):
+#         b = set(second[i]) - set(second[i-1])
+#         answer.append(b.pop())
     
-    return answer
+#     return answer
 
 # def solution(s):
 #     answer = []
@@ -43,4 +43,24 @@ def solution(s):
 #                 seen.add(num)
     
 #     return answer
+
+
+def solution(s):
+    # 문자열 파싱
+    tuples = s[2:-2].split("},{")
+    tuples = [list(map(int, t.split(","))) for t in tuples]
+
+    # 튜플의 길이에 따라 정렬
+    tuples.sort(key=lambda x: len(x))
+
+    # 결과를 저장할 리스트
+    answer = []
+
+    # 각 튜플을 구성하는 원소를 추가
+    for t in tuples:
+        for num in t:
+            if num not in answer:
+                answer.append(num)
+
+    return answer
 
