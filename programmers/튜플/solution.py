4b061e4f953c199da39a881e122fee9a1922980a
foresec <109335369+foresec@users.noreply.github.com>
2024-01-03 22:41:55 +0900

    updated at 2024. 1. 3. 오후 10:41:55

--- programmers/튜플/solution.py
+++ programmers/튜플/solution.py
@@ -1,46 +1,46 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=python3
 
-# # 붙여나감
-# def solution(s):
-#     answer = []
-#     # split을 잘하고
-#     first = s[2:-2].split("},{")
-#     second = []
-#     for f in first:
-#         a = list(map(int, f.split(",")))
-#         second.append(a)
-    
-#     # 가장 작은 길이부터 sort해서 not in 으로 채워나가는걸 찾는다??(append)
-#     second.sort(key= lambda x: len(x))
-    
-#     answer.append(second[0][0])
-#     for i in range(1, len(second)):
-#         b = set(second[i]) - set(second[i-1])
-#         answer.append(b.pop())
-    
-#     return answer
-
+# 붙여나감
 def solution(s):
     answer = []
-    
     # split을 잘하고
     first = s[2:-2].split("},{")
     second = []
-    seen = set()  
-
     for f in first:
         a = list(map(int, f.split(",")))
         second.append(a)
     
-    # 가장 작은 길이부터 sort
-    second.sort(key=lambda x: len(x))
-
-    # 중복 체크하면서 값을 추가
-    for i in range(len(second)):
-        for num in second[i]:
-            if num not in seen:
-                answer.append(num)
-                seen.add(num)
+    # 가장 작은 길이부터 sort해서 not in 으로 채워나가는걸 찾는다??(append)
+    second.sort(key= lambda x: len(x))
+    
+    answer.append(second[0][0])
+    for i in range(1, len(second)):
+        b = set(second[i]) - set(second[i-1])
+        answer.append(b.pop())
     
     return answer
+
+# def solution(s):
+#     answer = []
+    
+#     # split을 잘하고
+#     first = s[2:-2].split("},{")
+#     second = []
+#     seen = set()  
+
+#     for f in first:
+#         a = list(map(int, f.split(",")))
+#         second.append(a)
+    
+#     # 가장 작은 길이부터 sort
+#     second.sort(key=lambda x: len(x))
+
+#     # 중복 체크하면서 값을 추가
+#     for i in range(len(second)):
+#         for num in second[i]:
+#             if num not in seen:
+#                 answer.append(num)
+#                 seen.add(num)
+    
+#     return answer
 
