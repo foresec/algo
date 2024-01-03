659fe1307587eba3e2757875f25719293a4c4147
foresec <109335369+foresec@users.noreply.github.com>
2024-01-03 22:49:31 +0900

    updated at 2024. 1. 3. 오후 10:49:30

--- programmers/튜플/solution.py
+++ programmers/튜플/solution.py
@@ -15,37 +15,37 @@ def solution(s):
     
     answer.append(second[0][0])
     for i in range(1, len(second)):
-        b = set(second[i]) - set(second[i-1])
-        answer.append(b.pop())
-    
+        b = list(set(second[i]) - set(second[i-1]))
+        answer.append(b[0])
+
     return answer
 
-def solution(s):
-    answer = []
+# def solution(s):
+#     answer = []
     
-    # split을 잘하고
-    first = s[2:-2].split("},{")
-    second = []
+#     # split을 잘하고
+#     first = s[2:-2].split("},{")
+#     second = []
     
-    # set 하나에 저장해서 비교하는게 더 빠르다
-    seen = set()  
+#     # set 하나에 저장해서 비교하는게 더 빠르다
+#     seen = set()  
 
-    for f in first:
-        a = list(map(int, f.split(",")))
-        second.append(a)
+#     for f in first:
+#         a = list(map(int, f.split(",")))
+#         second.append(a)
     
-    # 가장 작은 길이부터 sort
-    second.sort(key=lambda x: len(x))
+#     # 가장 작은 길이부터 sort
+#     second.sort(key=lambda x: len(x))
 
-    # 중복 체크하면서 값을 추가
-    for i in range(len(second)):
-        for num in second[i]:
-            # set과 비교
-            if num not in seen:
-                answer.append(num)
-                seen.add(num)
+#     # 중복 체크하면서 값을 추가
+#     for i in range(len(second)):
+#         for num in second[i]:
+#             # set과 비교
+#             if num not in seen:
+#                 answer.append(num)
+#                 seen.add(num)
     
-    return answer
+#     return answer
 
 
 
