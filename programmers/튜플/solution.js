9279eaa48643bc7062de72be9bbeeac710d5eb50
foresec <109335369+foresec@users.noreply.github.com>
2024-01-03 23:12:44 +0900

    uploaded at 2024. 1. 3. 오후 11:12:44

---
+++ programmers/튜플/solution.js
@@ -0,0 +1,25 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=javascript
+
+function solution(s) {
+    let answer = [];
+    
+    lenS = s.length
+    let newS = s.substring(2, lenS-2).split("},{")
+    
+    tuples = []
+    for (t of newS) {
+        let temp = t.split(",").map(Number)
+        tuples.push(temp)
+    }
+    
+    tuples.sort((a, b) => a.length - b.length)
+    
+    answer.push(tuples[0][0])
+    
+    for (let i=1; i < tuples.length; i++) {
+        let diff = tuples[i].filter(item => !tuples[i-1].includes(item))
+        answer.push(diff[0])
+    }
+    
+    return answer;
+}
