664fd6679fffac8f7116826375acae57effc6d36
foresec <109335369+foresec@users.noreply.github.com>
2024-01-05 00:14:31 +0900

    uploaded at 2024. 1. 5. 오전 12:14:31

---
+++ programmers/프로세스/solution.js
@@ -0,0 +1,27 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=javascript
+
+function solution(priorities, location) {
+    let answer = 0;
+    
+    let len = priorities.length
+    let idx = 0
+    
+    while (true) {
+    
+        if (priorities[idx % len] == Math.max(...priorities)) {
+            // 최대를 0으로 갱신하며 지워가는 과정
+            priorities[idx % len] = 0
+            // 그 과정을 거치면서 count
+            answer++
+            
+            
+            // location과 같으면 목표지점이므로 break
+            if (location === idx % len) {
+                break
+            }
+        }
+
+        idx++
+    }
+    return answer;
+}
