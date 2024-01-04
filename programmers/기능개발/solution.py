570e9e4abd347505b365661c8d16cbc41538f7d6
foresec <109335369+foresec@users.noreply.github.com>
2024-01-04 14:21:45 +0900

    updated at 2024. 1. 4. 오후 2:21:45

--- programmers/기능개발/solution.py
+++ programmers/기능개발/solution.py
@@ -24,52 +24,52 @@
 #     return answer
 
 
-# def solution(progresses, speeds):
+def solution(progresses, speeds):
 
-#     answer = []
-#     time = 0
-#     count = 0
-#     while len(progresses)> 0:
-#         if (progresses[0] + time * speeds[0]) >= 100:
-#             progresses.pop(0)
-#             speeds.pop(0)
-#             count += 1
-#         else:
-#             if count > 0:
-#                 answer.append(count)
-#                 count = 0
-#             time += 1
-#     answer.append(count)
-#     return answer
+    answer = []
+    time = 0
+    count = 0
+    while len(progresses)> 0:
+        if (progresses[0] + time * speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
 
 
 # 각 배포마다 몇 개의 기능이 배포되는지 3
-def solution(progresses, speeds):
-    answer = []
+# def solution(progresses, speeds):
+#     answer = []
     
-    compare = []
-    for i in range(len(progresses)):
-        left = (100 - progresses[i]) // speeds[i]
+#     compare = []
+#     for i in range(len(progresses)):
+#         left = (100 - progresses[i]) // speeds[i]
         
-        if (100 - progresses[i]) % speeds[i] != 0:
-            left += 1
+#         if (100 - progresses[i]) % speeds[i] != 0:
+#             left += 1
             
-        compare.append(left)
+#         compare.append(left)
         
-    stack = []
+#     stack = []
     
-    while compare:
-        # 지금 판별하는 차례의 수가 더 크면 반영
-        if stack and max(stack) < compare[0]:
-            answer.append(len(stack))
-            stack = []
-        # 아니라면 stack에 더해주기            
-        else:
-            stack.append(compare.pop(0))
+#     while compare:
+#         # 지금 판별하는 차례의 수가 더 크면 반영
+#         if stack and max(stack) < compare[0]:
+#             answer.append(len(stack))
+#             stack = []
+#         # 아니라면 stack에 더해주기            
+#         else:
+#             stack.append(compare.pop(0))
 
-    # 남은 스택이 있다면 더해주기
-    if stack:
-        answer.append(len(stack))
+#     # 남은 스택이 있다면 더해주기
+#     if stack:
+#         answer.append(len(stack))
 
-    return answer
+#     return answer
 
