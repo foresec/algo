86ffc4af1fccb59537521fea73eba11ce564fb08
foresec <109335369+foresec@users.noreply.github.com>
2024-01-05 00:13:09 +0900

    updated at 2024. 1. 5. 오전 12:13:08

--- programmers/프로세스/solution.py
+++ programmers/프로세스/solution.py
@@ -1,82 +1,27 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42587
 
-# # location(idx)에 해당되는 프로세스가 몇번째로 실행되는지
-
-# from collections import deque
-
-# def solution(priorities, location):
+function solution(priorities, location) {
+    let answer = 0;
     
-#     answer = 0   
-#     q = deque(enumerate(priorities))
-
-#     while q:
-#         temp = q.popleft()
-#         if q:
-#             max_val = max(q, key=lambda x: x[1])
-        
-#         # 우선순위가 아닐때
-#         if max_val[1] > temp[1]:
-#             q.append(temp)
+    let len = priorities.length
+    let idx = 0
+    
+    while (true) {
+    
+        if (priorities[idx % len] == Math.max(...priorities)) {
+            // 최대를 0으로 갱신하며 지워가는 과정
+            priorities[idx % len] = 0
+            // 그 과정을 거치면서 count
+            answer++
             
-#         # 우선순위가 됐을 때
-#         else:
-#             # 프로세스 실행 카운트
-#             answer += 1
-#             if temp[0] == location:
-#                 break
-    
-#     return answer
-
-
-
-
-# def solution(priorities, location):
-#     jobs = len(priorities)
-#     answer = 0
-#     cursor = 0
-#     while True:
-#         if max(priorities) == priorities[cursor%jobs]:
-#             priorities[cursor%jobs] = 0
-#             answer += 1
-#             if cursor%jobs == location:
-#                 break
-#         cursor += 1   
-#     return answer
-
-
-
-
-
-
-
-
-
-
-# 우선 순위대로 실행
-# 실행되면 그냥 빠지고
-# 실행 안되면 뒤에 새로 넣음
-
-# location에 해당하는 프로세스는 몇번째로 실행되는가
-from collections import deque
-def solution(priorities, location):
-    
-    answer = 0   
-    q = deque(enumerate(priorities))
-    while q:
-        temp = q.popleft()
-        
-        if q:
-            max_val = max(q, key=lambda x: x[1])
-        
-        # 우선순위가 아닐때
-        if max_val[1] > temp[1]:
-            q.append(temp)
             
-        # 우선순위가 됐을 때
-        else:
-            # 프로세스 실행 카운트
-            answer += 1
-            if temp[0] == location:
+            // location과 같으면 목표지점이므로 break
+            if (location === idx % len) {
                 break
-        
-    return answer
+            }
+        }
+
+        idx++
+    }
+    return answer;
+}
