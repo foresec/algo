37af45868cd95d3fdbadbbe064d1d3b6114d08e8
foresec <109335369+foresec@users.noreply.github.com>
2024-01-04 14:24:59 +0900

    updated at 2024. 1. 4. 오후 2:24:58

--- programmers/기능개발/solution.py
+++ programmers/기능개발/solution.py
@@ -1,29 +1,6 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42586
 
-def solution(progresses, speeds):
-    answer = []
-    turn = 0
-    
-    while turn < len(progresses):
-        # 한 턴당 배포할 수 있는 수
-        cnt = 0  
-        
-        # 누적
-        for i in range(turn, len(progresses)):
-            progresses[i] += speeds[i]
-            
-        # 조건에 맞는 값 반영
-        while turn < len(progresses) and progresses[turn] >= 100:
-            turn += 1
-            cnt += 1
-
-        # 배포할 작업이 존재한다면 count
-        if cnt > 0:
-            answer.append(cnt)
-
-    return answer
-
-
+# 효율제일 좋음 
 # def solution(progresses, speeds):
 
 #     answer = []
@@ -43,7 +20,7 @@ def solution(progresses, speeds):
 #     return answer
 
 
-# 각 배포마다 몇 개의 기능이 배포되는지 3
+# 각 배포마다 몇 개의 기능이 배포되는지 32
 # def solution(progresses, speeds):
 #     answer = []
     
@@ -72,4 +49,24 @@ def solution(progresses, speeds):
 #         answer.append(len(stack))
 
 #     return answer
+
+
+def solution(progresses, speeds):
+    answer = []
+    stack = []
+
+    while progresses:
+        progresses = [p + s for p, s in zip(progresses, speeds)]
+        count = 0
+
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+
+        if count > 0:
+            answer.append(count)
+
+    return answer
+
 
