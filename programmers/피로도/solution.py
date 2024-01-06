ec1fa1635b3fc696b389a2b3e21fe739e3192336
foresec <109335369+foresec@users.noreply.github.com>
2024-01-06 23:35:43 +0900

    updated at 2024. 1. 6. 오후 11:35:43

--- programmers/피로도/solution.py
+++ programmers/피로도/solution.py
@@ -1,24 +1,26 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3
 
-
 def dfs(k, cnt, dungeons, visited):    
     global answer     
     
+    # 현재 answer보다 cnt가 더 많아지면 업데이트
     if cnt > answer:        
         answer = cnt	
         
-    for j in range(len(dungeons)):    	
-        if k >= dungeons[j][0] and not visited[j]:        	
-            visited[j] = 1            
-            dfs(k - dungeons[j][1], cnt + 1, dungeons, visited)            
-            visited[j] = 0  
+    for i in range(len(dungeons)):    
+        # 피로도 조건을 만족하고, 방문한적 없을 경우
+        if k >= dungeons[i][0] and not visited[i]:        	
+            visited[i] = 1            
+            dfs(k - dungeons[i][1], cnt + 1, dungeons, visited)            
+            visited[i] = 0  
             
 def solution(k, dungeons):    
     global answer
     answer = 0
-    len_d = len(dungeons)    
+    len_d = len(dungeons)
     visited = [0] * len_d
     
+    # 백트래킹 구현
     dfs(k, 0, dungeons, visited)  
     
     return answer
