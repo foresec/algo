f4f0c8648fa58389d7fee16f59e3380091e63945
foresec <109335369+foresec@users.noreply.github.com>
2024-01-09 23:15:33 +0900

    updated at 2024. 1. 9. 오후 11:15:33

--- programmers/타겟 넘버/solution.py
+++ programmers/타겟 넘버/solution.py
@@ -1,43 +1,44 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
-# def dfs(idx, total, numbers, target): 
-#     global answer
+answer = 0
+def dfs(idx, total, numbers, target): 
+    global answer
 
-#     if idx == len(numbers):
-#         if total == target:
-#             answer += 1
-#         return
+    if idx == len(numbers):
+        if total == target:
+            answer += 1
+        return
 
-#     dfs(idx + 1, total + numbers[idx], numbers, target)
-#     dfs(idx + 1, total - numbers[idx], numbers, target)
+    dfs(idx + 1, total + numbers[idx], numbers, target)
+    dfs(idx + 1, total - numbers[idx], numbers, target)
+
+
+def solution(numbers, target):
+    global answer
+    
+    # idx로 갯수 체크, total로 target 체크
+    dfs(0, 0, numbers, target)
+        
+    
+    return answer
 
 
 # def solution(numbers, target):
 #     answer = 0 
     
+#     def dfs(idx, total): 
+#         nonlocal answer
+        
+#         if idx == len(numbers):
+#             if total == target:
+#                 answer += 1
+#             return
+        
+#         dfs(idx + 1, total + numbers[idx])
+#         dfs(idx + 1, total - numbers[idx])
+    
 #     # idx로 갯수 체크, total로 target 체크
-#     dfs(0, 0, numbers, target)
+#     dfs(0, 0)
         
     
 #     return answer
-
-
-def solution(numbers, target):
-    answer = 0 
-    
-    def dfs(idx, total): 
-        nonlocal answer
-        
-        if idx == len(numbers):
-            if total == target:
-                answer += 1
-            return
-        
-        dfs(idx + 1, total + numbers[idx])
-        dfs(idx + 1, total - numbers[idx])
-    
-    # idx로 갯수 체크, total로 target 체크
-    dfs(0, 0)
-        
-    
-    return answer
