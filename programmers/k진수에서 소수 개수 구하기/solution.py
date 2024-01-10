8aca6d7486b2aa94c5bec35617cfbd8c662a817b
foresec <109335369+foresec@users.noreply.github.com>
2024-01-10 23:50:09 +0900

    updated at 2024. 1. 10. 오후 11:50:09

--- programmers/k진수에서 소수 개수 구하기/solution.py
+++ programmers/k진수에서 소수 개수 구하기/solution.py
@@ -1,29 +1,29 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/92335
 
+    
+def isPrime(a):
+    if (a < 2):
+        return False
+    for i in range(2, int(a**0.5)+1):
+        if (a % i == 0):
+            return False
+    return True
+
 def solution(n, k):
     answer = 0
     
     num = ''
     while n > 0:
-        temp = n % k
+        # temp = n % k
+        # n //= k
+        # num = str(temp) + num
+        num += str(n % k)
         n //= k
-        num = str(temp) + num
-    #     num += str(n % k)
-    #     n //= k
-    # num = num[::-1] 
-    
+    num = num[::-1] 
     
     num_list = list(num.split("0"))
-    
-    def isPrime(a):
-        if (a < 2):
-            return False
-        for i in range(2, int(a**0.5)+1):
-            if (a % i == 0):
-                return False
-        return True
+
         
-    
     # 일단 소수 구하고 에라토스테네스의 체
     for num in num_list:
         if len(num) != 0:
@@ -32,3 +32,44 @@ def solution(n, k):
         
                     
     return answer
+
+
+# 에라토스테네스의 체를 쓰니 런타임 에러가 일부에서 남    
+# def get_prime(a):
+#     primes = [True] * (a + 1)
+#     primes[0] = primes[1] = False
+#     for i in range(2, int(a ** 0.5)+ 1):
+#         if primes[i]:
+#             for j in range(i * i, a+1, i):
+#                 primes[j] = False
+#     result = []
+#     for x in range(a+1):
+#         if primes[x]:
+#             result.append(x)
+#     return result
+
+# def solution(n, k):
+#     answer = 0
+    
+#     num = ''
+#     while n > 0:
+#         temp = n % k
+#         n //= k
+#         num = str(temp) + num
+#     #     num += str(n % k)
+#     #     n //= k
+#     # num = num[::-1] 
+    
+#     num_list = list(num.split("0"))
+#     num_list = [int(x) for x in num_list if x]
+    
+#     max_num = max(num_list)
+#     prime_list = get_prime(max_num)
+        
+#     # 일단 소수 구하고 에라토스테네스의 체
+#     for num in num_list:
+#         if num in prime_list:
+#             answer += 1
+        
+                    
+#     return answer
