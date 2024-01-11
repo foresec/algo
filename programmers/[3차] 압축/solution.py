bbfd4e1ef452808b5f0ee13b2722b6c010c18179
foresec <109335369+foresec@users.noreply.github.com>
2024-01-12 00:16:09 +0900

    updated at 2024. 1. 12. 오전 12:16:09

--- programmers/[3차] 압축/solution.py
+++ programmers/[3차] 압축/solution.py
@@ -2,6 +2,7 @@
 
 def solution(msg):
     answer = []
+    len_msg = len(msg)
     # 1. chr, ord로 딕셔너리 만들기(미묘하게 조금 더 빠른듯?)
     str_dict = {}
     for i in range(26):
@@ -10,23 +11,21 @@ def solution(msg):
     
     # 2. zip함수를 이용해서 딕셔너리만들기 
     # str_dict = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", range(1,27)))
-        
-    n = 26    
+         
     i = 0
-    while i < len(msg):
+    while i < len_msg:
         # 기본적으로 i는 한칸씩 늘어날 예정
         check = 1  
         # 역으로 시작해야 최대한 뒤에서부터 긴문자열을 찾음
-        for j in range(len(msg), i, -1):
+        for j in range(len_msg, i, -1):
             strs = msg[i:j]
             if strs in str_dict.keys():
                 # 조건대로 색인번호(answer) 등록
                 answer.append(str_dict[strs])
                 
                 # 사전 업데이트
-                n += 1
                 next_str = msg[i:j+1]
-                str_dict[next_str] = n
+                str_dict[next_str] = len(str_dict) + 1
                 
                 # 앞으로 나가야하는만큼 더해줌
                 check = j - i
