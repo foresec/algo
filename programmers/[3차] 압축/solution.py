8b8e4de588a27e8987cf8f56bfd7e8e458291bec
foresec <109335369+foresec@users.noreply.github.com>
2024-01-12 00:13:29 +0900

    updated at 2024. 1. 12. 오전 12:13:29

--- programmers/[3차] 압축/solution.py
+++ programmers/[3차] 압축/solution.py
@@ -14,20 +14,26 @@ def solution(msg):
     n = 26    
     i = 0
     while i < len(msg):
+        # 기본적으로 i는 한칸씩 늘어날 예정
         check = 1  
+        # 역으로 시작해야 최대한 뒤에서부터 긴문자열을 찾음
         for j in range(len(msg), i, -1):
             strs = msg[i:j]
             if strs in str_dict.keys():
+                # 조건대로 색인번호(answer) 등록
                 answer.append(str_dict[strs])
                 
+                # 사전 업데이트
                 n += 1
                 next_str = msg[i:j+1]
                 str_dict[next_str] = n
                 
+                # 앞으로 나가야하는만큼 더해줌
                 check = j - i
+                
+                # 찾았다면 더이상 현재상황에서는 할게 없으니 break
                 break
                 
         i += check
-              
-        
+                      
     return answer
