640598fea96b18f9a99fa8a82a67afe11079e3b6
foresec <109335369+foresec@users.noreply.github.com>
2024-01-14 19:50:46 +0900

    updated at 2024. 1. 14. 오후 7:50:46

--- programmers/[3차] n진수 게임/solution.py
+++ programmers/[3차] n진수 게임/solution.py
@@ -7,12 +7,13 @@
 # t : 미리구할 숫자 갯수(길이네..)
 def solution(n, t, m, p):
     
+    # 맨 처음 0으로 시작
     answer = "0"
     num_dict = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
     
     for num in range(30000):
         temp = ""
-        # 진수더하기
+        # 진수 더하기
         while num > 0:
             na = num % n
             if na > 0:
@@ -25,15 +26,15 @@ def solution(n, t, m, p):
             num //= n
             
         answer += temp
-    
+        
+        if len(answer) > 1000 * m * p:
+            break
             
     temp2 = []
-    # 내 순서, 게임 인원 고려
+    # range로 내 순서, 게임 인원 고려
     for i in range(p-1, len(answer), m):
         temp2.append(answer[i])
         
     answer = "".join(temp2)
-    print(answer)
-    
-    
+    # print(answer)
     return answer[:t]
