a779d28043ebe4f098793121c24f2c6749e63cec
foresec <109335369+foresec@users.noreply.github.com>
2024-01-15 22:35:16 +0900

    updated at 2024. 1. 15. 오후 10:35:16

--- programmers/더 맵게/solution.py
+++ programmers/더 맵게/solution.py
@@ -1,23 +1,58 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42626
 
+# import heapq
+
+# def solution(scoville, K):
+    
+#     heapq.heapify(scoville)
+#     answer = 0
+
+#     while scoville[0] < K:
+        
+#         if len(scoville) < 2:
+#             return -1
+        
+#         min1 = heapq.heappop(scoville)
+#         min2 = heapq.heappop(scoville)
+#         new_scoville = min1 + (min2 * 2)
+        
+#         heapq.heappush(scoville, new_scoville)
+        
+#         answer += 1
+
+#     return answer
+
+
+
+
+
+
+
+
+
+
+
+# 다시풀기
 import heapq
 
 def solution(scoville, K):
-    
-    heapq.heapify(scoville)
     answer = 0
-
+    heapq.heapify(scoville)
+    
     while scoville[0] < K:
         
         if len(scoville) < 2:
-            return -1
+            answer = -1
+            break
         
-        min1 = heapq.heappop(scoville)
-        min2 = heapq.heappop(scoville)
-        new_scoville = min1 + (min2 * 2)
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
         
-        heapq.heappush(scoville, new_scoville)
-        
-        answer += 1
+        temp = a + b * 2
+        heapq.heappush(scoville, temp)
+                
+        answer += 1       
 
+        
+        
     return answer
