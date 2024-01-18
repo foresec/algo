917a82b803a755bee34c6fa6e3b7f04ff675daa4
foresec <109335369+foresec@users.noreply.github.com>
2024-01-18 23:45:40 +0900

    uploaded at 2024. 1. 18. 오후 11:45:40

---
+++ programmers/행렬의 덧셈/solution.py
@@ -0,0 +1,14 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+def solution(arr1, arr2):
+    
+    
+    N = len(arr1)
+    M = len(arr1[0])
+    answer = [[0] * M for _ in range(N)]
+    
+    for i in range(N):
+        for j in range(M):
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+            
+    return answer
