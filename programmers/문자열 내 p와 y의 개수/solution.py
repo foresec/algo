bd6575a90b7039f8c2d611ae6819712665f57629
foresec <109335369+foresec@users.noreply.github.com>
2024-01-22 23:51:15 +0900

    uploaded at 2024. 1. 22. 오후 11:51:15

---
+++ programmers/문자열 내 p와 y의 개수/solution.py
@@ -0,0 +1,9 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12916
+
+def solution(s):
+    answer = False
+    s = s.lower()
+    if s.count("p") == s.count("y"):
+        answer = True
+
+    return answer
