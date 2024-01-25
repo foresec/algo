bbf9310ebf90fc0d64f57ab05d99b452f1428359
foresec <109335369+foresec@users.noreply.github.com>
2024-01-25 23:17:54 +0900

    updated at 2024. 1. 25. 오후 11:17:54

--- programmers/핸드폰 번호 가리기/solution.py
+++ programmers/핸드폰 번호 가리기/solution.py
@@ -1,6 +1,7 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12948
 
 def solution(phone_number):
+    
     n = len(phone_number)-4
     answer = "*" * n + phone_number[-4:]
     return answer
