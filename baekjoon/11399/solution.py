699dbf88a4a9efd099ab00b63ed2e4bae0071973
foresec <109335369+foresec@users.noreply.github.com>
2024-02-27 23:41:32 +0900

    uploaded at 2024. 2. 27. 오후 11:41:31

---
+++ baekjoon/11399/solution.py
@@ -0,0 +1,16 @@
+// [문제 링크]: https://www.acmicpc.net/problem/11399
+
+N = int(input())
+arr = list(map(int, input().split()))
+​
+# 오름차순 정렬
+arr.sort()
+ans = [0] * N 
+​
+ans[0] = arr[0]
+​
+for i in range(1, N):
+    ans[i] = ans[i-1] + arr[i]
+​
+    
+print(sum(ans))
