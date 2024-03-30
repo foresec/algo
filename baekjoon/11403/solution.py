69f8e37370187556c1aae8704ad9fbb2a43baa1e
foresec <109335369+foresec@users.noreply.github.com>
2024-03-30 23:46:59 +0900

    updated at 2024. 3. 30. 오후 11:46:59

--- baekjoon/11403/solution.py
+++ baekjoon/11403/solution.py
@@ -4,17 +4,16 @@ import sys
 input = sys.stdin.readline
 ​
 N = int(input())
-adj_list = [[0 for j in range(N)] for _ in range(N)]
+adj = [list(map(int, input().split())) for _ in range(N)]
 ​
-for i in range(N):
-    adj_list[i] = list(map(int, input().split()))
 ​
 ​
 for k in range(N):
     for i in range(N):
         for j in range(N):
-            if adj_list[i][k] and adj_list[k][j]:
-                adj_list[i][j] = 1
+            if adj[i][k] and adj[k][j]:
+                adj[i][j] = 1
 ​
-for i in range(N):
-    print(*adj_list[i])
+for g in adj:
+    print(*g)
+​
