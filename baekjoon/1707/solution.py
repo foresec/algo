61537f0059158be146674f728c6126b87b06a13a
foresec <109335369+foresec@users.noreply.github.com>
2024-04-01 23:58:55 +0900

    updated at 2024. 4. 1. 오후 11:58:55

--- baekjoon/1707/solution.py
+++ baekjoon/1707/solution.py
@@ -4,47 +4,44 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
 ​
-import sys
-input = sys.stdin.readline
-​
-def dfs(n):
-    global total
+def dfs(n, adj, visited, check):
     visited[n] = True
-​
+    
     for nn in adj[n]:
 ​
         if not visited[nn]:
-            check[nn] = (check[n]+1) % 2
-            dfs(nn)
+            check[nn] = (check[n] + 1) % 2
+            dfs(nn, adj, visited, check)
 ​
         elif check[n] == check[nn]:
-            total = False
+            return False
+        
+    return True
 ​
-K= int(input())
+K = int(input())
 ​
 for _ in range(K):
-​
-    V, E = map(int, input().split())
-    adj = [[ ] for _ in range(V + 1)]
-    visited = [False]  * (V+1)
-    check = [0] * (V+1)
-    total = True
+    V, E = map(int, sys.stdin.readline().split())
+    adj = [[] for _ in range(V + 1)]
+    visited = [False] * (V + 1)
+    check = [0] * (V + 1)
+    ans = True
 ​
     for i in range(E):
-        s, e = map(int, input().split())
+        s, e = map(int, sys.stdin.readline().split())
         adj[s].append(e)
         adj[e].append(s)
-    
+​
 ​
     for i in range(1, V+1):
-        if total:
-            dfs(i)
+        if ans:
+            ans = dfs(i, adj, visited, check)
            
         else:
             # 이분그래프가 아니게 될 경우 반복을 멈춤
             break
     
-    if total:
+    if ans:
         print("YES")
     else:
         print("NO")
