4c5bae2b5b5f06c810065a609b46f6b401d3ea7b
foresec <109335369+foresec@users.noreply.github.com>
2024-06-20 14:58:50 +0900

    [level 2] Title: 짝지어 제거하기, Time: 79.39 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,19 +1,58 @@
-function solution(s) {
-
-    let stack = []
+// function solution(s) {
+//     let answer = 0
+//     let stack = []
     
-    for (let i = 0; i < s.length; i++) {
+    
+//     // stack을 써서 남아있는 바로 전 값과 비교
+//     for (alpha of s) {
         
-        if (stack.length > 0) {
-            let a = stack.pop() 
-            if (a !== s[i]) {
-                stack.push(a)
-                stack.push(s[i])
-            }
+//         if (stack && stack[stack.length-1] === alpha) {
+//             stack.pop()
+//         }
+//         else {
+//             stack.push(alpha)
+//         }
+//     }
+//     console.log(!stack.length)
+//     // stack이 남은 수가 없다면 성공
+//     if (!stack.length) {
+//         answer = 1
+//     }
+         
+//     return answer;
+// }
+
+
+// function solution(s) {
+
+//     let stack = []
+    
+//     for (let i = 0; i < s.length; i++) {
+        
+//         if (stack.length > 0) {
+//             let a = stack.pop() 
+//             if (a !== s[i]) {
+//                 stack.push(a)
+//                 stack.push(s[i])
+//             }
+//         } else {
+//             stack.push(s[i])
+//         }
+//     }
+    
+//     return stack.length === 0 ? 1:0
+// }
+
+function solution(s) {
+    const stack = [];
+
+    for (const alpha of s) {
+        if (stack.length > 0 && stack.at(-1) === alpha) {
+            stack.pop();
         } else {
-            stack.push(s[i])
+            stack.push(alpha);
         }
     }
-    
-    return stack.length === 0 ? 1:0
+
+    return stack.length === 0 ? 1 : 0;
 }
