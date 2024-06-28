21c34e5acab70fe9053db10fe6986e361a5016c6
foresec <109335369+foresec@users.noreply.github.com>
2024-06-28 11:48:46 +0900

    [Gold III] Title: 나무 재테크, Time: 1016 ms, Memory: 44436 KB -BaekjoonHub

---
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.js
@@ -0,0 +1,110 @@
+const dx = [1, 0, -1, 0, 1, 1, -1, -1];
+const dy = [0, -1, 0, 1, 1, -1, -1, 1];
+
+// 봄 : 나이만큼 양분을 먹고, 나이가 1 증가
+// 여러개의 나무가 있다면 나이가 어린 나무부터 먹으며, 나이만큼 못먹을시 즉사
+function spring() {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      let temp = tree[i][j];
+      temp.sort((a, b) => a - b);
+      let alive = [];
+      let dead = [];
+      for (let t = 0; t < temp.length; t++) {
+        let age = temp[t];
+        if (age <= food[i][j]) {
+          food[i][j] -= age;
+          alive.push(age + 1);
+        } else {
+          dead.push(age);
+        }
+      }
+      tree[i][j] = alive;
+      deadTrees[i][j] = dead;
+    }
+  }
+}
+
+// 여름 : 죽은 나무가 양분(나무의 나이 / 2, 소수점 아래는 버림)이됨
+function summer() {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      for (let t of deadTrees[i][j]) {
+        let val = Math.floor(t / 2);
+        food[i][j] += val;
+      }
+      deadTrees[i][j] = [];
+    }
+  }
+}
+
+// 가을 : 나이가 5의배수인 나무 번식, 주변 8개 칸에 나이가 1인 나무 생성
+function autumn() {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      for (let t of tree[i][j]) {
+        if (t % 5 === 0) {
+          for (let d = 0; d < 8; d++) {
+            let nx = i + dx[d];
+            let ny = j + dy[d];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            tree[nx][ny].push(1);
+          }
+        }
+      }
+    }
+  }
+}
+
+// 겨울 : 양분추가
+function winter() {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      food[i][j] += plus[i][j];
+    }
+  }
+}
+
+// K년이 지난 후 살아남은 나무의 수
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./16235.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [N, M, K] = input[0].split(" ").map(Number);
+const plus = [];
+const tree = Array.from({ length: N }, () =>
+  Array.from({ length: N }, () => [])
+);
+
+const food = Array.from({ length: N }, () =>
+  Array.from({ length: N }, () => 5)
+);
+
+const deadTrees = Array.from({ length: N }, () =>
+  Array.from({ length: N }, () => [])
+);
+
+for (let i = 1; i < N + 1; i++) {
+  plus.push(input[i].split(" ").map(Number));
+}
+
+for (let i = 1 + N; i < M + N + 1; i++) {
+  let [r, c, age] = input[i].split(" ").map(Number);
+  tree[r - 1][c - 1].push(age);
+}
+
+for (let k = 0; k < K; k++) {
+  spring();
+  summer();
+  autumn();
+  winter();
+}
+
+let ans = 0;
+for (const line of tree) {
+  for (const tline of line) {
+    ans += tline.length;
+  }
+}
+
+console.log(ans);
