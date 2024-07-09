d0aea1d340c122ff0265f142a07ba28e8a08e724
foresec <109335369+foresec@users.noreply.github.com>
2024-07-09 22:36:13 +0900

    [Gold IV] Title: 여왕벌, Time: 4276 ms, Memory: 134836 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.js
+++ 백준/Gold/10836. 여왕벌/여왕벌.js
@@ -1,60 +1,35 @@
-function growOnBorder(M, arr, values) {
-  let startX = M - 1;
-  let startY = 0;
-  let idx = 0;
-
-  while (idx < values.length) {
-    arr[startX][startY] += values[idx];
-
-    let nx = startX - 1;
-    let ny = startY;
-
-    if (nx < 0) {
-      nx = startX;
-      ny = startY + 1;
-    }
-    startX = nx;
-    startY = ny;
-    idx++;
-  }
-
-  return arr;
-}
-
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./10836.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [M, N] = input[0].split(" ").map(Number);
 
-let arr = Array(M)
-  .fill(0)
-  .map(() => Array(M).fill(1));
+let values = Array(2 * M - 1).fill(0);
 
-for (let i = 1; i < N + 1; i++) {
+for (let i = 1; i <= N; i++) {
   let [zero, one, two] = input[i].split(" ").map(Number);
-
-  let temp = Array(zero + one + two);
   let idx = 0;
 
   for (let j = 0; j < zero; j++) {
-    temp[idx++] = 0;
+    values[idx++] += 0;
   }
 
   for (let j = 0; j < one; j++) {
-    temp[idx++] = 1;
+    values[idx++] += 1;
   }
 
   for (let j = 0; j < two; j++) {
-    temp[idx++] = 2;
+    values[idx++] += 2;
   }
-
-  // 성장
-  arr = growOnBorder(M, arr, temp);
 }
 
-let initialValues = arr[0].slice(1)
-for (const row of arr) {
-	let ans = [row[0], ...initialValues]
-	console.log(ans.join(" "))
+for (let i = M - 1; i >= 0; i--) {
+  // 왼쪽열 채워넣으면서
+  let row = [];
+  row.push(values[i] + 1);
+  for (let j = M; j < 2 * M - 1; j++) {
+    // 나머지 열도 채워넣어 row완성 (나머지열은 최상단 열의 value와 같음)
+    row.push(values[j] + 1);
+  }
+  console.log(row.join(" "));
 }
