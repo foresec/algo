29f066c4ad64e89ef5d525c4d1d45ee423c1ca86
foresec <109335369+foresec@users.noreply.github.com>
2024-08-29 13:46:45 +0900

    [Gold IV] Title: 공유기 설치, Time: 236 ms, Memory: 38376 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.js
@@ -0,0 +1,41 @@
+function inHouse(houses, val, C) {
+  let cnt = 1;
+  let current = houses[0];
+
+  for (let i = 0; i < houses.length; i++) {
+    if (houses[i] - current >= val) {
+      cnt++;
+      current = houses[i];
+
+      // 조건을 달성하면 true
+      if (cnt === C) return true;
+    }
+  }
+  return false;
+}
+
+// 두 공유기 사이의 거리를 최대로
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./2110.txt";
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+const [N, C] = input[0].split(" ").map(Number);
+const houses = input.slice(1).map(Number);
+houses.sort((a, b) => a - b);
+
+let ans = Infinity;
+
+let left = 1;
+let right = 1000000000;
+
+while (left <= right) {
+  const mid = Math.floor((left + right) / 2);
+
+  if (inHouse(houses, mid, C)) {
+    left = mid + 1;
+    ans = mid;
+  } else {
+    right = mid - 1;
+  }
+}
+
+console.log(ans);
