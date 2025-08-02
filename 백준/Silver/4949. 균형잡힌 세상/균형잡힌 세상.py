b71626c25f3491c543eb6e959040ac754d925031
foresec <109335369+foresec@users.noreply.github.com>
2025-08-02 13:24:12 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+dict = {")": "(", "]": "["}
+
+while True:
+    s = input().rstrip()
+    if s == ".":
+        break
+
+    stack = []
+    ans = "yes"
+
+    for alpha in s:
+        if alpha in ["(", "["]:
+            stack.append(alpha)
+        elif alpha in [")", "]"]:
+            if stack and dict[alpha] == stack[-1]:
+                stack.pop(-1)
+            else:
+                stack.append(alpha)
+
+    if stack:
+        ans = "no"
+
+    print(ans)
