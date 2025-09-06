c32f43a9233d42f013b744b2e03674525d823c1e
foresec <109335369+foresec@users.noreply.github.com>
2025-09-06 17:28:02 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 136 ms, Memory: 115128 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,29 +1,31 @@
 import sys
+
 input = sys.stdin.readline
 
 T = int(input())
 
 for _ in range(T):
     N, M = map(int, input().split())
-    A = sorted(list(map(int, input().split())), reverse=True)
-    B = sorted(list(map(int, input().split())), reverse=True)
+    A = sorted(list(map(int, input().split())))
+    B = sorted(list(map(int, input().split())))
 
-    # 8 7 3 1 1
-    # 6 3 1
     cnt = 0
-
     a = 0
     b = 0
 
-    while a <N and b < M:
+    while a < N:
 
-        if A[a] > B[b]:
-            cnt += M - b
-            b = 0
+        # 비교물고기 비교가 끝까지 간 경우
+        if b == M:
+            cnt += b
             a += 1
-
-        elif A[a] <= B[b]:
-            b += 1
-   
+        else:
+            # 기준물고기가 더 크면 다음으로
+            if A[a] > B[b]:
+                b += 1
+            else:
+                # 작거나 같다면 다음 기준물고기로 변경 및 계산한거 반영
+                a += 1
+                cnt += b
 
     print(cnt)
