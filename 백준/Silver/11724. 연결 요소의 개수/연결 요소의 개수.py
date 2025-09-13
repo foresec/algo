23b8cb2c8a39b8f56b458104583edfa9c5cc0d16
foresec <109335369+foresec@users.noreply.github.com>
2025-09-13 14:29:32 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 672 ms, Memory: 67048 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+from collections import deque
+
+
+def bfs(n, val):
+    q = deque([n])
+    visited[n] = val
+
+    while q:
+        node = q.pop()
+        for i in adj_list[node]:
+            if visited[i] >= 1:
+                continue
+            q.append(i)
+            visited[i] = val
+
+
+N, M = map(int, input().split())
+
+adj_list = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    adj_list[u].append(v)
+    adj_list[v].append(u)
+
+
+visited = [0] * (N + 1)
+
+val = 0
+for n in range(1, N + 1):
+    val += 1
+    if not visited[n]:
+        bfs(n, val)
+
+ans = len(set(visited)) - 1
+print(ans)
