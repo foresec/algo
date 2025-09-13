544312a667168c15fc754050b3e0f03b925e5d11
foresec <109335369+foresec@users.noreply.github.com>
2025-09-13 14:31:31 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 648 ms, Memory: 67048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -31,9 +31,10 @@ visited = [0] * (N + 1)
 
 val = 0
 for n in range(1, N + 1):
-    val += 1
     if not visited[n]:
+        val += 1
         bfs(n, val)
 
 ans = len(set(visited)) - 1
+
 print(ans)
