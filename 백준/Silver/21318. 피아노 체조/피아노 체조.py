d006bf1fe67a265d84c35c212599a71ad4f9e212
foresec <109335369+foresec@users.noreply.github.com>
2025-09-27 01:37:56 +0900

    [Silver I] Title: 피아노 체조, Time: 308 ms, Memory: 61920 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.py
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.py
@@ -6,14 +6,17 @@ scores = list(map(int, input().split()))
 Q = int(input())
 queries = [list(map(int, input().split())) for _ in range(Q)]
 
-mistakes = [0] * N
-for i in range(N-1):
-    if scores[i] > scores[i+1]:
+difficulties = [0] + scores
+
+mistakes = [0] * (N + 1)
+for i in range(2, N + 1):
+    if difficulties[i] < difficulties[i - 1]: 
         mistakes[i] = 1
 
-prefix = [0] * (N+1)
-for i in range(1, N+1):
-    prefix[i] = prefix[i-1] + mistakes[i-1]
+prefix = [0] * (N + 1)
+for i in range(1, N + 1):
+    prefix[i] = prefix[i - 1] + mistakes[i]
+
 
 for x, y in queries:
-    print(prefix[y-1] - prefix[x-1])
+    print(prefix[y] - prefix[x])
