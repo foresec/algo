7d8f4a62cd3025f422bb0a085448af3c4d454090
foresec <109335369+foresec@users.noreply.github.com>
2025-10-04 14:59:13 +0900

    [Gold V] Title: 토마토, Time: 1340 ms, Memory: 146656 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,46 +1,55 @@
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
+
+import sys
+input = sys.stdin.readline
 
 from collections import deque
 
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
 
-def bfs(q):
-    
+
+def bfs(tom):
+    q = deque(tom)
+    day = 0
+
     while q:
-        x, y, val = q.popleft()
-        for d in range(4):
+        # 포인트는 포문으로 첫 시작점을 모두 돌려야 함
+        for _ in range(len(q)):
+            x, y, day = q.popleft()
+                   
+            for d in range(4):
+                nx = x + dx[d]
+                ny = y + dy[d]
 
-            nx = x + dx[d]
-            ny = y + dy[d]
+                if not (0 <= nx < N and 0 <= ny < M):
+                    continue
 
-            if nx < 0 or nx >= N or ny < 0 or ny >= M:
-                continue
+                if arr[nx][ny] == 0:
+                    q.append((nx, ny, day + 1))
+                    arr[nx][ny] = 1
 
-            if arr[nx][ny] == 0:
-                arr[nx][ny] = val + 1
-                q.append((nx, ny, val + 1))
+    return day
 
 
-# 토마토가 모두 익을 때까지의 최소 날짜
-# 토마토가 모두 익을 수 없다면 -1
 M, N = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
 ans = 0
 
-active = deque()
+tomato = []
 for i in range(N):
     for j in range(M):
+        # 토마토를 발견하면 익히기
         if arr[i][j] == 1:
-            active.append((i, j, 1))
+            # 시작 시점은 day = 0
+            tomato.append((i, j, 0))
 
-bfs(active)
+ans = bfs(tomato)
 
 for i in range(N):
     for j in range(M):
-        ans = max(ans, arr[i][j])
         if arr[i][j] == 0:
-            print(-1)
-            exit(0)
+            ans = -1
+            break
+        
 
-print(ans - 1)
+print(ans)
