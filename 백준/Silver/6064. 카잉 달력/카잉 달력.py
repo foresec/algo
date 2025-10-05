c90e4d0cd3cea3044964f1adec7f235b6a0aaf43
foresec <109335369+foresec@users.noreply.github.com>
2025-10-05 15:48:29 +0900

    [Silver I] Title: 카잉 달력, Time: 284 ms, Memory: 111448 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,24 @@
+
+import math
+
+
+def get_lcm(a, b):
+    if a == 0 or b == 0:
+        return 0
+    return a * b // math.gcd(a, b)
+
+
+T = int(input())
+for _ in range(T):
+    M, N, x, y = map(int, input().split())
+    ans = -1
+
+    limit = get_lcm(M, N)
+    val = x
+
+    while val <= limit:
+        if (val - y) % N == 0:
+            ans = val
+            break
+        val += M
+    print(ans)
